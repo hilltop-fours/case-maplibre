d9f6a8db9757f4dfc9c87ae70019cbe7059f31d8
Paul Berden <paul@paulberden.nl>
2024-05-28 10:59:13 +0200

    Upgrade to angular 18

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,20 +1,20 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { Map } from 'maplibre-gl';
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss'],
+    selector: 'app-map',
+    templateUrl: './map.component.html',
+    styleUrls: ['./map.component.scss'],
+    standalone: true,
 })
 export class MapComponent implements AfterViewInit {
 
-  @ViewChild('mapContainer')
-  private mapContainer: ElementRef | undefined;
-  private map: Map | null = null;
+  private mapContainer = viewChild<ElementRef>('mapContainer');
+  private map?: Map = undefined;
 
-  public ngAfterViewInit(): void {
+  ngAfterViewInit() {
     this.map = new Map({
-      container: this.mapContainer?.nativeElement,
+      container: this.mapContainer()?.nativeElement,
       zoom: 10,
       center: [5.127684, 52.0809856],
       style: 'https://api.maptiler.com/maps/streets/style.json?key=mEcMYH4LeHvlyd1FeMwK'
